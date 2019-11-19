c639d4703ea220bf29bf01540e052b3d17ce5ba4
hyeonji0409 <44424273+hyeonji0409@users.noreply.github.com>
2019-11-20 00:25:51 +0900

    Update PushPrint.java
    GitHub for git pull

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -10,6 +10,7 @@ public class PushPrint {
 		System.out.println("20183274 정현지입니다..");
 		System.out.println("현재 시각은 12시 06분 입니다.");
 		System.out.println("코드 수정 in GitHub");
+		System.out.println("World with Hatsune");
 	}
 
 }
