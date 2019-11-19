5b1738820023cd8dcd7c7a3f8eae192daa60111f
hyeonji0409 <guswl8507@naver.com>
2019-11-20 00:06:42 +0900

    pushPrint added code

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 		System.out.println("Add JAVA Class File Push");
 		System.out.println("20183274 정현지 입니다.");
+		System.out.println("현재 시각은 12시 06분 입니다.");
 	}
 
 }
