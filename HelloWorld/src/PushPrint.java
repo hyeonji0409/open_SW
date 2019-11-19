cbfd5b01d02ffa2961a461d81e9262683f6bd1d4
hyeonji0409 <guswl8507@naver.com>
2019-11-20 00:21:11 +0900

    PushPrint.java modified on Local repository

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -6,6 +6,7 @@ public class PushPrint {
 		System.out.println("Add JAVA Class File Push");
 		System.out.println("20183274 정현지 입니다.");
 		System.out.println("현재 시각은 12시 06분 입니다.");
+		System.out.println("코드 수정");
 	}
 
 }
